cffec28bceab2893ad41137b63d647ec0471da65
Thomas Meriaux <meriauxt@gmail.com>
2017-02-17 20:27:33 +0800

    Add files via upload
    Big update of the main components: 
    Changed the data structure for easier usability from list to dictionary for next part with data key comparisons
    Made the structure more modular for ease of checking

--- Examine frequency.py
+++ Examine frequency.py
@@ -1,81 +1,57 @@
-""" Frequency in a string is determined by the number of occurences/length of the string
-So first count the number of occurences for a letter and then divide it by len(string)Return the frequency
-"""
-import xlsxwriter
-import openpyxl
+
+
+index={}
 
 #data handling function
-def load_index_to_xls(filename,index):
-   
-#create the point handling function
-   def score(string):
-      score = 5
-      if string.find(letter)!=-1:
-         if string[letter][frequency] == index[string][letter][frequency]:
-            score +=5
-         elif (string[letter][frequency] - index[string][letter][frequency] <= 1):
-         score +=3
-
-         elif (string[letter][frequency] - index[string][letter][frequency] > 1):
-         score +=1
-
-      return score
-      
-         
-         
 
 #Create a file to put the data in
-def create_index_file():
-   workbook = xlsxwriter.Workbook('index.xlsx')
-   worksheet = workbook.add_worksheet()
-   worksheet.write('A1', 'Word')
-   workbook.close() 
 
 
 #Get frequency of a letter
 
 def frequency_return(string,letter):
     count=0
-    ranging_string=len(string)
+    
     for letters in string:
-        
        if letters==letter:
            count+=1
     return count
 
-index={}
-
-from openpyxl import load_workbook
-index = load_workbook('index.xlsx')
-
-
-
-
 #Scan all letters: if a letter has not been searched then count
-
-def store_words_and_frequency(string):
-    range_string=string
-    length_string=len(string)
-    datastore=[]
-    target=0
-    frequency=0
-    
-    if string not in index:
-        while len(range_string)!=0:
-            datastore.append(range_string[target])
+def get_frequency(string):
+  range_string=string
+  length_string=len(string)
+  datastore={}
+  target=0
+  frequency=0
+  while len(range_string)!=0:
+           # datastore.append(range_string[target])
             frequency = (int(frequency_return(range_string,range_string[target]))/length_string)
-            datastore.append(frequency)
+            frequency = format(frequency, '.2f')
+            datastore.update({range_string[target]:frequency})
             range_string = range_string.replace(range_string[target],'')
-            index.update({string: datastore})
-        return index
-    else:
-        return index
+  return datastore          
+          
+def index_string(string):
+  
+    if string not in index:
+      
+      index.update({string: (get_frequency(string))})
+    return index
     
-    
-store_words_and_frequency("string")
-store_words_and_frequency("string")
-store_words_and_frequency("boat")
-store_words_and_frequency("house")
+#basically what I want to achieve is compare the frequency between the two strings. I need to collect those frequencies from the first string. Once it is done several tests: Which string has the most identical letters and if there are several of them, match them against their respective probabilities
+
+
+def compare_frequency(name):
+  name_datastore=get_frequency(name)
+  
+  
+  
+
+index_string("string")
+index_string("string")
+index_string("balloon")
+index_string("drapeau")
 
 print(index)
 
